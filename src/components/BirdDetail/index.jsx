d6413e6c41fcfea2fb3c6c7375c0d9a69c3c90d1
Andy Bool <andybool50@gmail.com>
2024-10-08 14:35:15 +0100

    styling tweaks to recent section

--- src/components/BirdDetail/index.jsx
+++ src/components/BirdDetail/index.jsx
@@ -2,8 +2,8 @@ export default function BirdDetail({ key, name, image, location }) {
     return (
         <div className="basis-1/5 border rounded-lg">
                     <h3 className="p-2 text-xs font-bold">{name}</h3>
-                    <img src={image} alt={name} className="h-16 w-16 rounded-full"></img>
-                    <p className="p-2 text-xs">Seen at: {location}</p>
+                    <img src={image} alt={name} className="h-16 w-16 rounded-full mx-auto"></img>
+                    <p className="p-2 text-xs">{location}</p>
                 </div>
     )
 }
--- src/components/Recent/index.jsx
+++ src/components/Recent/index.jsx
@@ -17,7 +17,7 @@ export default function Recent() {
     return (
         <div>
             <h2 className="p-2">Recent sightings</h2>
-            <div className="flex gap-2 p-2 content-center">
+            <div className="flex gap-2 p-2">
                 {recents.map(recent => <BirdDetail
                     key={recent.id}
                     name={recent.name}
